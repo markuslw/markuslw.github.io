b78427cb77c330b9822d692f625b641906170492
Markus Wilhelmsen <markuslw10@gmail.com>
2026-01-04 21:21:03 +0100

    additions and fixes

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,6 +5,7 @@ import github from './assets/imgs/github.svg';
 import uitlogo_black from './assets/imgs/uit_logo_black.png';
 import uitlogo_white from './assets/imgs/uit_logo_white.png';
 import elkjoplogo from './assets/imgs/elkjop_logo.svg';
+import tmlogo from './assets/imgs/tm_logo.jpg';
 import pb from './assets/imgs/pb.jpg';
 import emoji from "react-easy-emoji";
 import en from './assets/locales/en.json';
@@ -12,33 +13,52 @@ import no from './assets/locales/no.json';
 
 function App() {
 
-  const [language, setLanguage] = useState('en');
-  const [darkmode, setDarkmode] = useState(false);
+  const [language, setLanguage] = useState(() => {
+    const saved = localStorage.getItem('lang');
+    return saved || 'en';
+  });
+  const [darkmode, setDarkmode] = useState(() => {
+    const saved = localStorage.getItem('darkmode');
+    return saved === 'true' || false;
+  });
 
   const content = language === 'en' ? en : no;
 
   const toggleLanguage = () => {
     const next = language === 'en' ? 'no' : 'en';
     setLanguage(next);
-    localStorage.setItem('preferredLanguage', next);
+    localStorage.setItem('lang', next);
   };
 
   useEffect(() => {
     if (darkmode) {
       document.documentElement.setAttribute('data-theme', 'dark');
+      localStorage.setItem('darkmode', 'true');
     } else {
       document.documentElement.removeAttribute('data-theme');
+      localStorage.setItem('darkmode', 'false');
     }
   }, [darkmode]);
 
   return (
     <div>
       <button className="button" onClick={toggleLanguage}>
-        {language === 'en' ? 'Norsk' : 'English'}
+          <svg className="control" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+            <path d="M4.545 6.714 4.11 8H3l1.862-5h1.284L8 8H6.833l-.435-1.286zm1.634-.736L5.5 3.956h-.049l-.679 2.022z"/>
+            <path d="M0 2a2 2 0 0 1 2-2h7a2 2 0 0 1 2 2v3h3a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-3H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zm7.138 9.995q.289.451.63.846c-.748.575-1.673 1.001-2.768 1.292.178.217.451.635.555.867 1.125-.359 2.08-.844 2.886-1.494.777.665 1.739 1.165 2.93 1.472.133-.254.414-.673.629-.89-1.125-.253-2.057-.694-2.82-1.284.681-.747 1.222-1.651 1.621-2.757H14V8h-3v1.047h.765c-.318.844-.74 1.546-1.272 2.13a6 6 0 0 1-.415-.492 2 2 0 0 1-.94.31"/>
+          </svg>
       </button>
 
       <button className="button" onClick={() => setDarkmode(!darkmode)}>
-        {darkmode ? 'Light Mode' : 'Dark Mode'}
+        {darkmode ?
+          <svg className="control" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+            <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0M8.5 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0m0 11a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0m5-5a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1m-11 0a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1m9.743-4.036a.5.5 0 1 1-.707-.707.5.5 0 0 1 .707.707m-7.779 7.779a.5.5 0 1 1-.707-.707.5.5 0 0 1 .707.707m7.072 0a.5.5 0 1 1 .707-.707.5.5 0 0 1-.707.707M3.757 4.464a.5.5 0 1 1 .707-.707.5.5 0 0 1-.707.707"/>
+          </svg>
+          :
+          <svg className="control" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+            <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708"/>
+          </svg>
+        }
       </button>
       <div className="welcome">
         <div className="intro">
@@ -47,19 +67,19 @@ function App() {
             <hr className="separator-small" />
             <div className="icon-container">
               {/* LINKEDIN SVG */}
-              <a className="no-style" href="https://www.linkedin.com/in/markuswilhelmsen/">
+              <a href="https://www.linkedin.com/in/markuswilhelmsen/">
                 <svg className="icon" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                   <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
                 </svg>
               </a>
               {/* GITHUB SVG */}
-              <a className="no-style" href="https://github.com/markuslw">
+              <a href="https://github.com/markuslw">
                 <svg className="icon" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                   <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
                 </svg>
               </a>
               {/* CV PDF */}
-              <a className="no-style" href="/cv_markuswilhelmsen.pdf" download>
+              <a href="/cv_markuswilhelmsen.pdf" download>
                 <svg className="icon" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
                   <path fill-rule="evenodd" d="M14 4.5V14a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5zM1.6 11.85H0v3.999h.791v-1.342h.803q.43 0 .732-.173.305-.175.463-.474a1.4 1.4 0 0 0 .161-.677q0-.375-.158-.677a1.2 1.2 0 0 0-.46-.477q-.3-.18-.732-.179m.545 1.333a.8.8 0 0 1-.085.38.57.57 0 0 1-.238.241.8.8 0 0 1-.375.082H.788V12.48h.66q.327 0 .512.181.185.183.185.522m1.217-1.333v3.999h1.46q.602 0 .998-.237a1.45 1.45 0 0 0 .595-.689q.196-.45.196-1.084 0-.63-.196-1.075a1.43 1.43 0 0 0-.589-.68q-.396-.234-1.005-.234zm.791.645h.563q.371 0 .609.152a.9.9 0 0 1 .354.454q.118.302.118.753a2.3 2.3 0 0 1-.068.592 1.1 1.1 0 0 1-.196.422.8.8 0 0 1-.334.252 1.3 1.3 0 0 1-.483.082h-.563zm3.743 1.763v1.591h-.79V11.85h2.548v.653H7.896v1.117h1.606v.638z"/>
                 </svg>
@@ -67,13 +87,13 @@ function App() {
             </div>
             <div className="column">
               {/* PHONE SVG */}
-              <a href="tel:+4741559061" className="no-style">
+              <a href="tel:+4741559061">
                 <svg className="phone" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
                   <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
                 </svg>
               </a>
               {/* MESSAGE SVG */}
-              <a href="sms:+4741559061" className="no-style">
+              <a href="sms:+4741559061">
                 <svg className="message" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
                   <path d="M8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6-.097 1.016-.417 2.13-.771 2.966-.079.186.074.394.273.362 2.256-.37 3.597-.938 4.18-1.234A9 9 0 0 0 8 15"/>
                 </svg>
@@ -83,7 +103,7 @@ function App() {
             </div>
             <div className="column">
               {/* EMAIL SVG */}
-              <a href="mailto:markuslw10@icloud.com" className="no-style">
+              <a href="mailto:markuslw10@icloud.com">
                 <svg className="email" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
                   <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z"/>
                 </svg>
@@ -93,7 +113,7 @@ function App() {
             </div>
             <div className="column">
               {/* LOCATION SVG */}
-              <a href="geo:69.6489, 18.95508" className="no-style">
+              <a href="geo:69.6489, 18.95508">
                 <svg className="location" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
                   <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>
                 </svg>
@@ -127,6 +147,7 @@ function App() {
 
         <h2 className="title">{content.experience.title}</h2>
         <hr className="separator" />
+
         {/* UIT */}
         <div className="column">
           <img className="icon invertimg" src={uitlogo_black} alt="Logo" />
@@ -165,6 +186,24 @@ function App() {
           {content.experience.elkjop.description}
         </p>
 
+        <br />
+
+        {/* TEKNIKMAGASINET */}
+        <div className="column">
+          <img className="icon icon-extras" src={tmlogo} alt="Logo" />
+          <h3>Teknikmagasinet Norge</h3>
+        </div>
+        <div className="indent">
+          <div className="table">
+            <p>Seller</p>
+            <p className="right">2021-2023</p>
+          </div>
+        </div>
+        <p className="indent-large text">
+          Lorem Ipsum dolor sit amet, ... , etc.
+        </p>
+
+        
         <br />
 
         <h2 className="title">{content.projects.title}</h2>
